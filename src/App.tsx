d35cbfe761ca4b6d50998fc0df682cc74983438e
BenMW03 <benmw03@gmail.com>
2025-12-03 18:40:36 -0500

    Good to go

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import GridPicker from "./components/GridPicker";
 
 function App() {
   return (
-    <head>
-      <meta name="google-adsense-account" content="ca-pub-1359140865107399"></meta>
     <div className="bg-gray-100 min-h-screen p-4">
       <GridPicker />
     </div>
-    </head>
   );
 }
 
